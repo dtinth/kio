8bd059c841a73d2aa95cd38d8802dc07227b36dc
dtinth on MBP M1 <dtinth@spacet.me>
2024-09-29 02:26:01 +0700

    must use 127.0.0.1?

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "pnpm preview",
-    url: "http://localhost:4322",
+    url: "http://127.0.0.1:4322",
     reuseExistingServer: true,
   },
 });
